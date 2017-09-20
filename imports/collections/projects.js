0a119403d659addabd41cdcb8566d8e1ab4c22a0
Martin Aspeli <optilude@gmail.com>
2017-09-20 23:51:01 +0100

    Add more robust validation and tests thereof

--- imports/collections/projects.js
+++ imports/collections/projects.js
@@ -28,114 +28,212 @@ export const ThroughputType = {
     none: "none"
 };
 
+export const ErrorTypes = {
+    shouldBeSmaller: "shouldBeSmaller",
+    shouldBeGreater: "shouldBeGreater"
+}
+
+SimpleSchema.setDefaultMessages({
+    messages: {
+        en: {
+            [ErrorTypes.shouldBeSmaller]: "{{{label}}} must be smaller than the high guess",
+            [ErrorTypes.shouldBeGreater]: "{{{label}}} must be greater than the low guess",
+        },
+    },
+});
+
 export const Backlog = new SimpleSchema({
 
     // backlog size
-    lowGuess: { type: Integer, min: 0 },
-    highGuess: { type: Integer, min: 0 },
-    lowSplitRate: { type: Number, min: 1 },
-    highSplitRate: { type: Number, min: 1 },
+
+    "lowGuess": { type: Integer, min: 0, custom: function() {
+        if(this.value > this.siblingField('highGuess').value) {
+            return ErrorTypes.shouldBeSmaller;
+        }
+    } },
+
+    "highGuess": { type: Integer, min: 0, custom: function() {
+        if(this.value < this.siblingField('lowGuess').value) {
+            return ErrorTypes.shouldBeGreater;
+        }
+    } },
+    
+    // probability of a backlog item being split (e.g. 1.2 means 20% of the time, a work item is split into two work items)
+
+    "lowSplitRate": { type: Number, min: 1, custom: function() {
+        if(this.value > this.siblingField('highSplitRate').value) {
+            return ErrorTypes.shouldBeSmaller;
+        }
+    } },
+
+    "highSplitRate": { type: Number, min: 1, custom: function() {
+        if(this.value < this.siblingField('lowSplitRate').value) {
+            return ErrorTypes.shouldBeGreater;
+        }
+    } },
 
     // possible risks that could increase scope
-    risks: { type: Array, optional: true },
+
+    "risks": { type: Array, optional: true },
     
-    'risks.$': Object,
-    'risks.$.name': String,
-    'risks.$.description': { type: String, optional: true },
-    'risks.$.likelihood': { type: Number, min: 0, max: 1}, // percentage
-    'risks.$.lowImpact': { type: Integer, min: 0 },  // number of work items added if risk hits (low guess)
-    'risks.$.highImpact': { type: Integer, min: 0 }, // number of work items added if risk hits (high guess)
+    "risks.$": Object,
+    "risks.$.name": String,
+    "risks.$.description": { type: String, optional: true },
+    "risks.$.likelihood": { type: Number, min: 0, max: 1},
+
+    "risks.$.lowImpact": { type: Integer, min: 0, custom: function() {
+        if(this.value > this.siblingField('highImpact').value) {
+            return ErrorTypes.shouldBeSmaller;
+        }
+    } },
+
+    "risks.$.highImpact": { type: Integer, min: 0, custom: function() {
+        if(this.value < this.siblingField('lowImpact').value) {
+            return ErrorTypes.shouldBeGreater;
+        }
+    } },
 
 });
 
 export const Team = new SimpleSchema({
 
-    members: { type: Array, optional: true },
-    'members.$': Object,
-    'members.$.role': String,
-    'members.$.description': { type: String, optional: true },
-    'members.$.quantity': { type: Number, min: 0 },
+    // who is on the team?
+    
+    "members": { type: Array, optional: true },
+    "members.$": Object,
+    "members.$.role": String,
+    "members.$.description": { type: String, optional: true },
+    "members.$.quantity": { type: Number, min: 0 },
 
-    // throughput calculations
-    throughputType: { type: String, optional: true, allowedValues: Object.values(ThroughputType) }, // are we using samples or estimates?
+    // how is throughput forecast (estimate, samples or none)
+    
+    "throughputType": { type: String, optional: true, allowedValues: Object.values(ThroughputType) },
 
-    // team's historical throughput
-    throughputSamples: { type: Array, optional: true },
-    'throughputSamples.$': Object,
-    'throughputSamples.$.periodStartDate': Date, // e.g. start of week or sprint
-    'throughputSamples.$.description': { type: String, optional: true },
-    'throughputSamples.$.throughput': Integer, // number of work items during this period
+    // team's historical throughput (start date of period, number of items in period)
+    
+    "throughputSamples": { type: Array, optional: true, custom: function() {
+        if((!this.value || this.value.length === 0) && this.siblingField('throughputType').value === ThroughputType.samples) {
+            return SimpleSchema.ErrorTypes.REQUIRED;
+        }
+    } },
 
-    // guess of team's throughput (will be used if there are no historical samples)
-    throughputEstimate: { type: Object, optional: true },
-    'throughputEstimate.lowGuess': { type: Integer, min: 0 }, // work items per period (low guess)
-    'throughputEstimate.highGuess': { type: Integer, min: 0 }, // work items per period (high guess)
+    "throughputSamples.$": Object,
+    "throughputSamples.$.periodStartDate": Date,
+    "throughputSamples.$.description": { type: String, optional: true },
+    "throughputSamples.$.throughput": Integer,
 
-    // S-curve scaling
-    rampUp: { type: Object, optional: true },
-    'rampUp.duration': { type: Number, min: 0 }, // number of periods of ramp up
-    'rampUp.throughputScalingLowGuess': { type: Number, min: 0, max: 1 }, // work items per period (low guess)
-    'rampUp.throughputScalingHighGuess': { type: Number, min: 0, max: 1 }, // work items per period (high guess)
+    // guess of team's throughput (work items per period)
 
-    // team's work pattern (will be used if there are no samples or throughput guesses)
-    workPattern: { type: Array, optional: true },
-    'workPattern.$': Object,
-    'workPattern.$.startDate': Date,
-    'workPattern.$.endDate': Date,
-    'workPattern.$.description': { type: String, optional: true }
+    "throughputEstimate": { type: Object, optional: true, custom: function() {
+        if(!this.value && this.siblingField('throughputType').value === ThroughputType.estimate) {
+            return SimpleSchema.ErrorTypes.REQUIRED;
+        }
+    } },
+
+    "throughputEstimate.lowGuess": { type: Integer, min: 0, custom: function() {
+        if(this.value > this.siblingField('highGuess').value) {
+            return ErrorTypes.shouldBeSmaller;
+        }
+    } },
+
+    "throughputEstimate.highGuess": { type: Integer, min: 0, custom: function() {
+        if(this.value > this.siblingField('highGuess').value) {
+            return ErrorTypes.shouldBeGreater;
+        }
+    } },
+
+    // S-curve scaling (applied during beginning of both sample and guess based throughput)
+    
+    "rampUp": { type: Object, optional: true },
+    "rampUp.duration": { type: Number, min: 0 },
+
+    "rampUp.throughputScalingLowGuess": { type: Number, min: 0, max: 1, custom: function() {
+        if(this.value > this.siblingField('throughputScalingHighGuess').value) {
+            return ErrorTypes.shouldBeSmaller;
+        }
+    } },
+
+    "rampUp.throughputScalingHighGuess": { type: Number, min: 0, max: 1, custom: function() {
+        if(this.value < this.siblingField('throughputScalingLowGuess').value) {
+            return ErrorTypes.shouldBeGreater;
+        }
+    } },
+
+    // team's work pattern
+    
+    "workPattern": { type: Array, optional: true, custom: function() {
+        if(!this.value && this.siblingField('throughputType').value === ThroughputType.none) {
+            return SimpleSchema.ErrorTypes.REQUIRED;
+        }
+    } },
+
+    "workPattern.$": Object,
+    "workPattern.$.startDate": Date,
+    "workPattern.$.endDate": Date,
+    "workPattern.$.description": { type: String, optional: true }
 
 });
 
 
 export const Solution = new SimpleSchema({
 
-    _id: String,
-    name: { type: String, min: 1 },
-    description: { type: String, optional: true },
-    notes: { type: String, optional: true },
+    "_id": String,
+    "name": { type: String, min: 1 },
+    "description": { type: String, optional: true },
+    "notes": { type: String, optional: true },
 
-    estimateType: { type: String, allowedValues: Object.values(EstimateType) },
+    // how do we estimate? 
+    "estimateType": { type: String, allowedValues: Object.values(EstimateType) },
 
-    throughputPeriodLength: { type: Integer, optional: true, min: 1, custom: function() {
-        if(!_.isInteger(this.value) && this.siblingField('estimateType').value === EstimateType.backlog) {
+    // how long is a period (weeks)
+    "throughputPeriodLength": { type: Integer, optional: true, min: 1, custom: function() {
+        if(!this.value && this.siblingField('estimateType').value === EstimateType.backlog) {
             return SimpleSchema.ErrorTypes.REQUIRED;
         }
     } },
 
-    startType: { type: String, allowedValues: Object.values(StartType) },
+    // when does work on the solution start?
+    "startType": { type: String, allowedValues: Object.values(StartType) },
 
-    startDate: { type: Date, optional: true, custom: function() {
+    "startDate": { type: Date, optional: true, custom: function() {
         if(!this.value && this.siblingField('startType').value === StartType.fixedDate) {
             return SimpleSchema.ErrorTypes.REQUIRED;
         }
     } },
-    
-    startDependency: { type: String, optional: true, custom: function() {
+
+    "startDependency": { type: String, optional: true, custom: function() {
         if(!this.value && (this.siblingField('startType').value === StartType.with || this.siblingField('startType').value === StartType.after)) {
             return SimpleSchema.ErrorTypes.REQUIRED;
         }
     } },
 
-    backlog: { type: Backlog, optional: true },
-    team: Team
+    // what is the backlog (used if `estimateType` is `backlog`)
+    "backlog": { type: Backlog, optional: true, custom: function() {
+        if(!this.value && this.siblingField('estimateType').value === EstimateType.backlog) {
+            return SimpleSchema.ErrorTypes.REQUIRED;
+        }
+    } },
+
+    // what is the team?
+    "team": Team
     
 });
 
 export const Project = new SimpleSchema({
-    _id: String,
+    "_id": String,
 
     // security
-    owner: String,
-    readOnlyShares: [String],
-    readWriteShares: [String],
+    "owner": String,
+    "readOnlyShares": [String],
+    "readWriteShares": [String],
 
     // basic metadata
-    name: String,
-    description: { type: String, optional: true },
-    startDate: Date,
+    "name": String,
+    "description": { type: String, optional: true },
+    "startDate": Date,
 
     // list of solutions
-    solutions: [Solution]
+    "solutions": [Solution]
 });
 
 /**
--- imports/collections/projects.spec.js
+++ imports/collections/projects.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { Project, newProject, Solution, newSolution } from './projects';
+import { Project, newProject, Solution, newSolution, EstimateType, ThroughputType, StartType } from './projects';
 
 describe('Project factory', function() {
     
@@ -31,3 +31,479 @@ describe('Solution factory', function() {
     });
 
 });
+
+describe('Validation', function() {
+    
+    it("Throughput period is required if estimate type is backlog", function() {
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                throughputPeriodLength: null
+            }))
+        }).to.throw();
+
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.workPattern,
+                throughputPeriodLength: null
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Start date is required if start type is fixed date", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.fixedDate,
+                startDate: null
+            }))
+        }).to.throw();
+
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.fixedDate,
+                startDate: new Date(2017, 0, 1)
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Does not require start date or dependency if solution starts immediately", function() {
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.immediately,
+                startDate: null,
+                startDependency: null
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Start dependency is required if start type is start-with", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.with,
+                startDependency: null
+            }))
+        }).to.throw();
+
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.with,
+                startDependency: 'id123'
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Start dependency is required if start type is start-after", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.after,
+                startDependency: null
+            }))
+        }).to.throw();
+
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                startType: StartType.after,
+                startDependency: 'id123'
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Backlog is required if estimate type is backlog", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: null
+            }))
+        }).to.throw();
+
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 1,
+                    highSplitRate: 1   
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Backlog size high guess must be greater than low guess", function() {
+       
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 2,
+                    highGuess: 1,
+                    lowSplitRate: 1,
+                    highSplitRate: 1   
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 1,
+                    highGuess: 2,
+                    lowSplitRate: 1,
+                    highSplitRate: 1   
+                }
+            }))
+        }).not.to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 2,
+                    highGuess: 2,
+                    lowSplitRate: 1,
+                    highSplitRate: 1   
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Backlog split rate high guess must be greater than low guess", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 2.1,
+                    highSplitRate: 1.2  
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 1.2,
+                    highSplitRate: 2.1   
+                }
+            }))
+        }).not.to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 1.2,
+                    highSplitRate: 1.2  
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Backlog risk impact high guess must be greater than low guess", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 1,
+                    highSplitRate: 1,
+                    risks: [{name: "A", likelihood: 0.5, lowImpact: 5, highImpact: 2}]
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 1,
+                    highSplitRate: 1,
+                    risks: [{name: "A", likelihood: 0.5, lowImpact: 2, highImpact: 5}]
+                }
+            }))
+        }).not.to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                backlog: {
+                    lowGuess: 0,
+                    highGuess: 0,
+                    lowSplitRate: 1,
+                    highSplitRate: 1,
+                    risks: [{name: "A", likelihood: 0.5, lowImpact: 2, highImpact: 2}]
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Throughput samples are required if throughput type is samples", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.samples,
+                    throughputSamples: null,
+                    throughputEstimate: null,
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.samples,
+                    throughputSamples: [],
+                    throughputEstimate: null,
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).to.throw();
+
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.samples,
+                    throughputSamples: [{periodStartDate: new Date(2017, 0, 1), throughput: 1}],
+                    throughputEstimate: null,
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Throughput estimate is required if throughput type is estimate", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: null,
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 1,
+                        highGuess: 1  
+                    },
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+    it("Throughput high guess must be greater than low guess ", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 2,
+                        highGuess: 1
+                    },
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 1,
+                        highGuess: 2
+                    },
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).not.to.throw();
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 1,
+                        highGuess: 1  
+                    },
+                    rampUp: null,
+                    workPattern: []
+                }
+            }))
+        }).not.to.throw();
+        
+    });
+
+    it("Ramp up throughput scaling high guess must be greater than low guess ", function() {
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 1,
+                        highGuess: 1
+                    },
+                    rampUp: {
+                        duration: 1,
+                        throughputScalingLowGuess: 0.2,
+                        throughputScalingHighGuess: 0.1
+                    },
+                    workPattern: []
+                }
+            }))
+        }).to.throw();
+
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 1,
+                        highGuess: 1
+                    },
+                    rampUp: {
+                        duration: 1,
+                        throughputScalingLowGuess: 0.1,
+                        throughputScalingHighGuess: 0.2
+                    },
+                    workPattern: []
+                }
+            }))
+        }).not.to.throw();
+        
+        expect(() => {
+            Solution.validate(newSolution({
+                name: "Test solution",
+                estimateType: EstimateType.backlog,
+                team: {
+                    members: [],
+                    throughputType: ThroughputType.estimate,
+                    throughputSamples: [],
+                    throughputEstimate: {
+                        lowGuess: 1,
+                        highGuess: 1
+                    },
+                    rampUp: {
+                        duration: 1,
+                        throughputScalingLowGuess: 0.2,
+                        throughputScalingHighGuess: 0.2
+                    },
+                    workPattern: []
+                }
+            }))
+        }).not.to.throw();
+
+    });
+
+
+});
+    
